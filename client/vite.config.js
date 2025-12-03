67ba2c3ef4dc879b12de7a48efd0d1e6e4c0d44c
Joris <joris_willrodt@yahoo.de>
2025-12-03 13:27:20 +0100

    client data

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import flowbite from 'flowbite/plugin'
+import { fileURLToPath } from 'url';
 import path from 'path'
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   base: '/',
@@ -12,7 +16,7 @@ export default defineConfig({
   },
   server: {
     host: true, // ← wichtig für externen Zugriff
-    allowedHosts: ['client-production-d8d9.up.railway.app'], // ← Railway-Domain freigeben
+    allowedHosts: ['localhost', '127.0.0.1'], // ← Railway-Domain freigeben
     proxy: {
       '/api': {
         target: 'http://localhost:3001',
